374a0685b28c6232d0b826ddebb649e7a675bded
pranavbadhan8 <pranavbadhan@example.com>
2023-07-15 22:06:13 +0530

    submitting code-Sat Jul 15 2023 22:06:10 GMT+0530 (India Standard Time)

---
+++ src/components/Input.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { ReactDOM } from "react";
+import { useState } from "react";
+
+function Input(){
+
+   let [name,setName] = useState("");
+
+    const inputName = (event)=>{
+        setName(event.target.value)
+    }
+
+   return(
+    <div>
+
+    <p>Enter your name:</p>  
+    <input type="text" className="name" onChange={inputName}/>
+    <p>{name}</p>
+    </div>
+   )
+}
+
+export default Input;
